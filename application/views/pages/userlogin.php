770a174f58f9a67e4c6b7874a02fa1e95be987d2
Sahil-Chavan <sahiledupor@gmail.com>
2022-05-07 16:45:25 +0530

    Basic Configuration
    Setup Basic routings and Configurations

---
+++ application/views/pages/userlogin.php
@@ -0,0 +1,75 @@
+
+<!-- <nav class="navbar navbar-light" style="background-color: #e3f2fd; height:20vh;"> -->
+
+
+<!-- <div class="container-fluid justify-content-center"> -->
+    <!-- <span class="navbar-brand mb-0 fs-1">Qube Health Assignment Project</span>  -->
+
+    <!-- <span class="align-bottom" style="height:100%;">By - Sahil S Chavan</span> -->
+    <!-- By - Sahil S Chavan -->
+  <!-- </div> -->
+  
+    
+
+<!-- <div class="min-vh-100"> -->
+    <!-- Column 1 -->
+  <div class="container py-5 h-100">
+    <div class="row d-flex align-items-center justify-content-center h-100">
+      <div class="col-md-8 col-lg-7 col-xl-6">
+        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+          class="img-fluid" alt="Phone image">
+      </div>
+
+      <!-- Column 2 -->
+      <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+          <dix class="row align-items-center" style="padding-top:10rem;">
+
+      <div class="container-fluid justify-content-center">
+          <h1 class="text-center">User Login</h1>
+            </div>
+        <form>
+          
+          <div class="form-outline mb-4">
+            <input type="email" id="form1Example13" class="form-control form-control-lg" />
+            <label class="form-label" for="form1Example13">Phone Number</label>
+          </div>
+
+          <!-- <div class="d-flex justify-content-around align-items-center mb-4">
+            
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="form1Example3" checked />
+              <label class="form-check-label" for="form1Example3"> Remember me </label>
+            </div>
+            <a href="#!">Forgot password?</a>
+          </div> -->
+
+          <!-- Submit button -->
+          <div class="d-flex justify-content-around align-items-center mb-4">
+          <button type="submit" class="btn btn-primary btn-lg btn-block">Send OTP</button>
+            </div>
+
+          <!-- <div class="divider d-flex align-items-center my-4">
+            <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+          </div>
+          <div class="container-fluid justify-content-center">
+          <h1 class="text-center">User Login</h1>
+            </div>
+        <form>
+          
+          <div class="form-outline mb-4">
+            <input type="email" id="form1Example13" class="form-control form-control-lg" />
+            <label class="form-label" for="form1Example13">Phone Number</label>
+          </div>
+
+          <div class="d-flex justify-content-around align-items-center mb-4">
+          <button type="submit" class="btn btn-primary btn-lg btn-block">Send OTP</button>
+            </div> -->
+          
+
+        </form>
+
+        </dix>
+      </div>
+    </div>
+  </div>
+<!-- </div> -->
---
+++ application/views/templates/footer.php
@@ -0,0 +1,43 @@
+<footer class="text-center text-white fixed-bottom" style="background-color: #e3f2fd;">
+  <!-- Grid container -->
+  <div class="container pt-1">
+    <!-- Section: Social media -->
+    <section>
+
+      <!-- Linkedin -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="https://www.linkedin.com/in/sahil-chavan/"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="bi bi-linkedin"></i></a>
+      <!-- Github -->
+      <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="https://www.github.com/Sahil-Chavan"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="bi-github" role="img" aria-label="GitHub"></i></a>
+        <a
+        class="btn btn-link btn-floating btn-lg text-dark m-1"
+        href="#!"
+        role="button"
+        data-mdb-ripple-color="dark"
+        ><i class="bi bi-link-45deg"></i></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <!-- <div class="text-center text-dark p-3" style="background-color: rgba(0, 0, 0, 0.2);"> -->
+  <div class="text-center text-dark p-2" >
+    Made By - Sahil S Chavan
+  </div>
+  <!-- Copyright -->
+</footer>
+
+
+
+</body>
+</html>
